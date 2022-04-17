3fef8e492407c7d63a7c6aa433db27803eba0f79
GemmiH <nunnaphat.hl@gmail.com>
2022-04-17 12:16:33 +0700

    helloooooooo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
 import {SmileOutlined} from '@ant-design/icons';
 
 function App() {
@@ -38,20 +38,40 @@ function App() {
        
     </Row>
       <Row>
+
+
         <Col span={24}
           style={{backgroundColor: "#6CC6F5",
             color: "#pOff00",
             fontsize: "65px" ,
-            fontFamily: "cursive",
-            paddingLeft: "25px",
-            fontWeight: "800",
+            //fontFamily: "Comic Sans",
+            //paddingLeft: "25px",
+            fontWeight: "600",
             textAlign: "left", rightalign: "leftborder(2.05)",
             borderRadius: "10px",
             margin: "10px",
           }}
         >
-        Poll
+          <Row style={{height: "70px"}}></Row>
+          <Row style={{backgroundColor: "#ffffff",
+            height: "70px",
+            fontSize: "25px",
+            paddingLeft: "20px", 
+            paddingTop: "10px"
+          }}>
+            Hamburger or Pizza?
+          </Row>
+          <Row style={{height: "190px",
+            paddingTop:"30px",
+            justifyContent:"center",
+          }}>
+            <Button style={{height:"60px",borderRadius:"10px"}}><h1>🍔</h1></Button>
+            <Button style={{height:"60px",borderRadius:"10px"}}><h1>🍕</h1></Button>
+          </Row>
       </Col>
+
+
+
     </Row>
 
     </div>
